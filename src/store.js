f00a80b51913369e4e580f2af51a89a8db40a2b6
Alexandre Gaudencio <shahor@shahor.fr>
2017-01-19 18:07:06 +0100

    Remove useless stuff

--- src/store.js
+++ src/store.js
@@ -1,16 +1,5 @@
 const worker = new Worker('/lib/store_worker.js')
 
-worker.postMessage({
-	type : 'mouse_move',
-	data : {
-		coords : { x : Math.random(), y : Math.random() }
-	}
-})
-
 export const pushData = (data) => {
 	worker.postMessage(data)
-}
-
-export default {
-
 }
