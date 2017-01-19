4575a08f92f0c9cc98f788486af1edfb4aee6d7b
Alexandre Gaudencio <shahor@shahor.fr>
2017-01-19 18:06:23 +0100

    Update store.js
    Indentation much?

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 const worker = new Worker('/lib/store_worker.js')
 
-	worker.postMessage({
-		type : 'mouse_move',
-		data : {
-			coords : { x : Math.random(), y : Math.random() }
-		}
-	})
-
+worker.postMessage({
+	type : 'mouse_move',
+	data : {
+		coords : { x : Math.random(), y : Math.random() }
+	}
+})
 
 export const pushData = (data) => {
 	worker.postMessage(data)
